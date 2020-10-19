7a92f662e992f8fc4f02ead2095e2b4cf69aa2b9
Tabrez Ansari <tabrezsvce@gmail.com>
2020-10-19 13:33:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hs Edi <code>src/App.js</code> and save to reload.
+                 Hs Edi <code>src/App.js
+    </code> and save to reload.
         </p>
+    
         <a
           className="App-link"
           href="https://reactjs.org"
